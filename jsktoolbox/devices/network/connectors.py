765a8223a74ce3a7c2a08340287a697b3e8ec5c6
Jacek Kotlarski <szumak@virthost.pl>
2023-08-25 12:41:20 +0200

    connectors template

---
+++ jsktoolbox/devices/network/connectors.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+"""
+  Author:  Jacek Kotlarski --<szumak@virthost.pl>
+  Created: 25.08.2023
+
+  Purpose:
+"""
+
+
+# #[EOF]#######################################################################
