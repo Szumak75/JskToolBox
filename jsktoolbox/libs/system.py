7a7c309e7b754e47d0f35b760f9060d91ab41a64
Jacek Kotlarski <szumak@virthost.pl>
2023-09-06 12:03:44 +0200

    PathChecker class and tests.

--- jsktoolbox/libs/system.py
+++ jsktoolbox/libs/system.py
@@ -5,8 +5,12 @@
 
   Purpose: various classes of interaction with the system.
 """
+
 import os
+import inspect
 from jsktoolbox.attribtool import NoDynamicAttributes
+from jsktoolbox.raisetool import Raise
+from jsktoolbox.libs.base_data import BData
 
 
 class Env(NoDynamicAttributes):
@@ -23,6 +27,56 @@ class Env(NoDynamicAttributes):
     def username(cls) -> str:
         """Return login name."""
         return os.getenv("USER")
+
+
+class PathChecker(BData, NoDynamicAttributes):
+    """PathChecker class for filesystem path."""
+
+    def __init__(self, pathname: str) -> None:
+        """Constructor."""
+        if pathname is None:
+            raise Raise.error(
+                "pathname as string expected, not None.",
+                TypeError,
+                self.__class__.__name__,
+                inspect.currentframe(),
+            )
+        if not isinstance(pathname, str):
+            raise Raise.error(
+                f"pathname as string expected, '{type(pathname)}' received.",
+                TypeError,
+                self.__class__.__name__,
+                inspect.currentframe(),
+            )
+        if isinstance(pathname, str) and len(pathname) == 0:
+            raise Raise.error(
+                "pathname cannot be an empty string.",
+                ValueError,
+                self.__class__.__name__,
+                inspect.currentframe(),
+            )
+        self.data["pathname"] = pathname
+        # analysis
+        self.__run__()
+
+    def __run__(self) -> None:
+        """Path analysis procedure."""
+
+    @property
+    def exists(self) -> bool:
+        """Return path exists flag."""
+
+    @property
+    def isdir(self) -> bool:
+        """Return path isdir flag."""
+
+    @property
+    def isfile(self) -> bool:
+        """Return path isfile flag."""
+
+    def create(self) -> bool:
+        """Create path procedure."""
+        return False
 
 
 # #[EOF]#######################################################################
