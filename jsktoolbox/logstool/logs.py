fec37d29caeda511fd37008e0d1de9c5a6ca5596
Jacek Kotlarski <szumak@virthost.pl>
2023-10-09 23:33:25 +0200

    LoggerEngine class

--- jsktoolbox/logstool/logs.py
+++ jsktoolbox/logstool/logs.py
@@ -186,6 +186,53 @@ class LoggerEngineSyslog(ILoggerEngine, BData, NoDynamicAttributes):
         """Send message to SYSLOG."""
 
 
+class LoggerEngine(BData, NoDynamicAttributes):
+    """LoggerEngine container class."""
+
+    __ckey = "conf"
+    __nkey = "noconf"
+
+    def __init__(self) -> None:
+        """Constructor."""
+        self.data[self.__nkey] = {}
+        self.data[self.__nkey][LogsLevelKeys.info] = [LoggerEngineStdout()]
+        self.data[self.__nkey][LogsLevelKeys.debug] = [LoggerEngineStderr()]
+
+    def add_engine(self, log_level: str, engine: ILoggerEngine) -> None:
+        """Add LoggerEngine to specific log level."""
+        if not isinstance(log_level, str):
+            raise Raise.error(
+                f"Key as string expected, '{type(log_level)}' received.'",
+                TypeError,
+                self.__class__.__name__,
+                currentframe(),
+            )
+        if not isinstance(engine, ILoggerEngine):
+            raise Raise.error(
+                f"ILoggerEngine type expected, '{type(engine)}' received.",
+                TypeError,
+                self.__class__.__name__,
+                currentframe(),
+            )
+        if self.__ckey not in self.data:
+            self.data[self.__ckey] = {}
+            self.data[self.__ckey][log_level] = [engine]
+        else:
+            if log_level not in self.data[self.__ckey].keys():
+                self.data[self.__ckey][log_level] = [engine]
+            else:
+                test = False
+                for i in range(0, len(self.data[self.__ckey][log_level])):
+                    if (
+                        self.data[self.__ckey][log_level][i].__class__
+                        == engine.__class__
+                    ):
+                        self.data[self.__ckey][log_level][i] = engine
+                        test = True
+                if not test:
+                    self.data[self.__ckey][log_level].append(engine)
+
+
 class LoggerClient(BData, NoDynamicAttributes):
     """Logger Client main class."""
 
