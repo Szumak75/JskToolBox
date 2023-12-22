e6aeaf7c121d4292d04143f122795561dd56f3bd
Jacek Kotlarski <szumak@virthost.pl>
2023-12-22 12:53:03 +0100

    empty element modules added

---
+++ jsktoolbox/devices/mikrotik/elements/tool.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+"""
+  tool.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 22.12.2023, 12:40:33
+  
+  Purpose: 
+"""
+
+from typing import Dict, List, Optional, Union, Tuple, Any
+from inspect import currentframe
+
+from jsktoolbox.attribtool import ReadOnlyClass
+from jsktoolbox.raisetool import Raise
+from jsktoolbox.logstool.logs import LoggerClient, LoggerQueue
+
+
+from jsktoolbox.devices.mikrotik.base import BRouterOS, BDev, Element
+from jsktoolbox.devices.network.connectors import IConnector
+
+
+class _Elements(object, metaclass=ReadOnlyClass):
+    """Keys definition class.
+
+    For internal purpose only.
+    """
+
+
+# #[EOF]#######################################################################
