6ef04d3be6b6f952d5448f59e00221d06358f981
Jacek Kotlarski <szumak@virthost.pl>
2024-03-20 16:00:11 +0100

    update interface elements

--- jsktoolbox/devices/mikrotik/elements/interface.py
+++ jsktoolbox/devices/mikrotik/elements/interface.py
@@ -31,6 +31,7 @@ class _Elements(object, metaclass=ReadOnlyClass):
     """
 
     ACCESS_LIST: str = "access-list"
+    ACTIVE: str = "active"  # v7
     ALIGN: str = "align"
     APN: str = "apn"
     BGP_VPLS: str = "bgp-vpls"
@@ -40,25 +41,40 @@ class _Elements(object, metaclass=ReadOnlyClass):
     CAP: str = "cap"
     CHANNELS: str = "channels"
     CISCO_BGP_VPLS: str = "cisco-bgp-vpls"
+    CLIENT: str = "client"  # v7
     CONNECT_LIST: str = "connect-list"
     DEVICE: str = "device"
+    DOT1X: str = "dot1x"  # v7
     EOIP: str = "eoip"
+    EOIPV6: str = "eoipv6"  # v7
     ESIM: str = "esim"
     ETHERNET: str = "ethernet"
+    FDB: str = "fdb"
     FILTER: str = "filter"
+    GRE6: str = "gre6"  # v7
     GRE: str = "gre"
     HOST: str = "host"
     INFO: str = "info"
     INTERWORKING_PROFILES: str = "interworking-profiles"
     IPIP: str = "ipip"
+    IPIPV6: str = "ipipv6"  # v7
+    L2TP_CLIENT: str = "l2tp-client"
+    L2TP_ETHER: str = "l2tp-ether"  # v7
+    L2TP_SERVER: str = "l2tp-server"
+    LIST: str = "list"
     LTE: str = "lte"
+    MACSEC: str = "macsec"  # v7
     MANUAL_TX_POWER_TABLE: str = "manual-tx-power-table"
     MDB: str = "mdb"
+    MEMBER: str = "member"
+    MESH: str = "mesh"
     MSTI: str = "msti"
     MST_OVERRIDE: str = "mst-override"
     NAT: str = "nat"
     NSTREME: str = "nstreme"
     NSTREME_DUAL: str = "nstreme-dual"
+    OVPN_CLIENT: str = "ovpn-client"
+    OVPN_SERVER: str = "ovpn-server"
     PACKET: str = "packet"
     PEERS: str = "peers"
     POE: str = "poe"
@@ -66,22 +82,39 @@ class _Elements(object, metaclass=ReadOnlyClass):
     PORT_CONTROLLER: str = "port-controller"
     PORT_EXTENDER: str = "port-extender"
     PORT_ISOLATION: str = "port-isolation"
+    PPPOE_CLIENT: str = "pppoe-client"
+    PPPOE_SERVER: str = "pppoe-server"
+    PPP_CLIENT: str = "ppp-client"
+    PPP_SERVER: str = "ppp-server"
+    PPTP_CLIENT: str = "pptp-client"
+    PPTP_SERVER: str = "pptp-server"
+    PROFILE: str = "profile"  # v7
     REGISTRATION_TABLE: str = "registration-table"
     ROOT: str = "interface"
     RULE: str = "rule"
     SECURITY_PROFILES: str = "security-profiles"
+    SERVER: str = "server"  # v7
     SETTINGS: str = "settings"
     SNIFFER: str = "sniffer"
     SNOOPER: str = "snooper"
+    SSTP_CLIENT: str = "sstp-client"
+    SSTP_SERVER: str = "sstp-server"
+    STATE: str = "state"  # v7
     STATION: str = "station"
     SWITCH: str = "switch"
+    VETH: str = "veth"  # v7
+    VIRTUAL_ETHERNET: str = "virtual-ethernet"
     VLAN: str = "vlan"
     VPLS: str = "vpls"
     VRRP: str = "vrrp"
+    VRRP: str = "vrrp"
+    VTEPS: str = "vteps"  # v7
+    VXLAN: str = "vxlan"  # v7
     W60G: str = "w60g"
     WDS: str = "wds"
-    WIREGUARD: str = "wireguard"
+    WIREGUARD: str = "wireguard"  # v7
     WIRELESS: str = "wireless"
+    _6TO4_: str = "6to4"  # v7
 
 
 class RBInterface(BRouterOS):
@@ -110,6 +143,7 @@ class RBInterface(BRouterOS):
 
         # add elements
         elements: Dict[str, Any] = {
+            _Elements._6TO4_: {},
             _Elements.BONDING: {},
             _Elements.BRIDGE: {
                 _Elements.CALEA: {},
@@ -131,7 +165,12 @@ class RBInterface(BRouterOS):
                 _Elements.SETTINGS: {},
                 _Elements.VLAN: {},
             },
+            _Elements.DOT1X: {
+                _Elements.CLIENT: {},
+                _Elements.SERVER: {_Elements.ACTIVE: {}, _Elements.STATE: {}},
+            },
             _Elements.EOIP: {},
+            _Elements.EOIPV6: {},
             _Elements.ETHERNET: {
                 _Elements.SWITCH: {
                     _Elements.HOST: {},
@@ -143,19 +182,42 @@ class RBInterface(BRouterOS):
                 _Elements.POE: {},
             },
             _Elements.GRE: {},
+            _Elements.GRE6: {},
             _Elements.IPIP: {},
+            _Elements.IPIPV6: {},
+            _Elements.L2TP_CLIENT: {},
+            _Elements.L2TP_ETHER: {},
+            _Elements.L2TP_SERVER: {_Elements.SERVER: {}},
+            _Elements.LIST: {_Elements.MEMBER: {}},
             _Elements.LTE: {
                 _Elements.APN: {},
                 _Elements.ESIM: {},
                 _Elements.SETTINGS: {},
             },
+            _Elements.MACSEC: {_Elements.PROFILE: {}},
+            _Elements.MESH: {_Elements.FDB: {}, _Elements.PORT: {}},
+            _Elements.OVPN_CLIENT: {},
+            _Elements.OVPN_SERVER: {_Elements.SERVER: {}},
+            _Elements.PPP_CLIENT: {},
+            _Elements.PPP_SERVER: {},
+            _Elements.PPPOE_CLIENT: {},
+            _Elements.PPPOE_SERVER: {_Elements.SERVER: {}},
+            _Elements.SSTP_CLIENT: {},
+            _Elements.SSTP_SERVER: {_Elements.SERVER: {}},
+            _Elements.VETH: {},
+            _Elements.VIRTUAL_ETHERNET: {},
             _Elements.VLAN: {},
             _Elements.VPLS: {
                 _Elements.BGP_VPLS: {},
                 _Elements.CISCO_BGP_VPLS: {},
             },
+            _Elements.VRRP: {},
+            _Elements.VXLAN: {_Elements.FDB: {}, _Elements.VTEPS: {}},
             _Elements.WIREGUARD: {
                 _Elements.PEERS: {},
+            },
+            _Elements.W60G: {
+                _Elements.STATION: {},
             },
             _Elements.WIRELESS: {
                 _Elements.ACCESS_LIST: {},
@@ -175,9 +237,6 @@ class RBInterface(BRouterOS):
                 },
                 _Elements.SNOOPER: {},
                 _Elements.WDS: {},
-            },
-            _Elements.W60G: {
-                _Elements.STATION: {},
             },
         }
 
--- tests/connector_test.py
+++ tests/connector_test.py
@@ -7,6 +7,7 @@
   Purpose:
 """
 
+from ast import Add
 from typing import Optional
 
 from jsktoolbox.netaddresstool.ipv4 import Address
@@ -20,8 +21,9 @@ from jsktoolbox.devices.mikrotik.base import Element
 
 if __name__ == "__main__":
     ch = API(
-        ip_address=Address("10.5.5.254"),
+        # ip_address=Address("10.5.5.254"),
         # ip_address=Address("10.255.53.118"),
+        ip_address=Address("10.1.15.36"),
         port=8728,
         login="devel",
         password="mojehaslo",
@@ -29,19 +31,21 @@ if __name__ == "__main__":
     ch.connect()
     q = LoggerQueue()
     query = RBQuery()
-    query.add_attrib("list", "allowed-devices3")
-    query.add_attrib("address", "10.30.32.9")
+    # query.add_attrib("list", "allowed-devices3")
+    # query.add_attrib("address", "10.30.32.9")
+    query.add_attrib("state", "Full")
     print(query.query)
 
     rb = RouterBoard(connector=ch, qlog=q, debug=True, verbose=True)
     # rb.dump()
     # print("Check element return")
     # out = rb.element("/ip/firewall/address-list/")
-    out: Optional[Element] = rb.element("/ip/firewall/address-list/", auto_load=True)
+    # out: Optional[Element] = rb.element("/ip/firewall/address-list/", auto_load=True)
+    out: Optional[Element] = rb.element("/routing/ospf/neighbor/", auto_load=True)
 
     if out:
         print(out.search(query.query))
-        # out.dump()
+        out.dump()
         # print(out)
         # print(out.root)
         # print(out.get())
