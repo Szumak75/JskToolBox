3bc404be29ca870aab43fefa093da3c7ee960812
Jacek Kotlarski <szumak@virthost.pl>
2024-10-07 14:11:43 +0200

    [edmctool] add StarsSystem container class

---
+++ jsktoolbox/edmctool/logs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+  logs.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 7.10.2024, 13:39:19
+  
+  Purpose: 
+"""
+
+
+# #[EOF]#######################################################################
