5e88fd2afa9d81f1a00538fc97d52a7a933ac9b0
Jacek 'Szumak' Kotlarski <szumak@virthost.pl>
2025-02-22 19:12:55 +0100

    `Refactor clipboard classes to use a base class and platform-specific implementations`

--- jsktoolbox/tktool/tools.py
+++ jsktoolbox/tktool/tools.py
@@ -31,6 +31,11 @@ class _IClip(ABC):
     def set_clipboard(self, value: str) -> None:
         """Set clipboard content."""
 
+    @property
+    @abstractmethod
+    def is_tool(self) -> bool:
+        """Check if tool is available."""
+
 
 class _Keys(object, metaclass=ReadOnlyClass):
     """Keys container class."""
@@ -45,76 +50,214 @@ class _Keys(object, metaclass=ReadOnlyClass):
     WINDOWS: str = "Windows"
 
 
-class _WinClip(BData, _IClip):
+class _BClip(BData, _IClip):
+    """Clipboard data class."""
+
+    def get_clipboard(self) -> str:
+        """Get clipboard content."""
+        if self.is_tool:
+            return self._get_data(key=_Keys.PASTE)()  # type: ignore
+        return ""
+
+    def set_clipboard(self, value: str) -> None:
+        """Set clipboard content."""
+        if self.is_tool:
+            self._get_data(key=_Keys.COPY)(value)  # type: ignore
+
+    @property
+    def is_tool(self) -> bool:
+        """Return True if the tool is available."""
+        return (
+            self._get_data(key=_Keys.COPY) is not None
+            and self._get_data(key=_Keys.PASTE) is not None
+        )
+
+
+class _WinClip(_BClip):
     """Windows clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
+    def __init__(self) -> None:
+        """Initialize the class."""
 
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+        if os.name == _Keys.NT or platform.system() == _Keys.WINDOWS:
+            get_cb = self.__win_get_clipboard
+            set_cb = self.__win_set_clipboard
+            self._set_data(
+                key=_Keys.COPY, value=set_cb, set_default_type=Optional[MethodType]
+            )
+            self._set_data(
+                key=_Keys.PASTE, value=get_cb, set_default_type=Optional[MethodType]
+            )
+
+    def __win_get_clipboard(self) -> str:
+        """Get windows clipboard data."""
+        ctypes.windll.user32.OpenClipboard(0)  # type: ignore
+        p_contents = ctypes.windll.user32.GetClipboardData(1)  # type: ignore # 1 is CF_TEXT
+        data = ctypes.c_char_p(p_contents).value
+        # ctypes.windll.kernel32.GlobalUnlock(p_contents)
+        ctypes.windll.user32.CloseClipboard()  # type: ignore
+        return data  # type: ignore
+
+    def __win_set_clipboard(self, text: str) -> None:
+        """Set windows clipboard data."""
+        text = str(text)
+        G_MEM_DDE_SHARE = 0x2000
+        ctypes.windll.user32.OpenClipboard(0)  # type: ignore
+        ctypes.windll.user32.EmptyClipboard()  # type: ignore
+        try:
+            # works on Python 2 (bytes() only takes one argument)
+            hCd = ctypes.windll.kernel32.GlobalAlloc(  # type: ignore
+                G_MEM_DDE_SHARE, len(bytes(text)) + 1  # type: ignore
+            )
+        except TypeError:
+            # works on Python 3 (bytes() requires an encoding)
+            hCd = ctypes.windll.kernel32.GlobalAlloc(  # type: ignore
+                G_MEM_DDE_SHARE, len(bytes(text, "ascii")) + 1
+            )
+        pchData = ctypes.windll.kernel32.GlobalLock(hCd)  # type: ignore
+        try:
+            # works on Python 2 (bytes() only takes one argument)
+            ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text))  # type: ignore
+        except TypeError:
+            # works on Python 3 (bytes() requires an encoding)
+            ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text, "ascii"))
+        ctypes.windll.kernel32.GlobalUnlock(hCd)  # type: ignore
+        ctypes.windll.user32.SetClipboardData(1, hCd)  # type: ignore
+        ctypes.windll.user32.CloseClipboard()  # type: ignore
 
 
-class _MacClip(BData, _IClip):
+class _MacClip(_BClip):
     """MacOS clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
+    def __init__(self) -> None:
+        """Initialize the class."""
+        if os.name == _Keys.MAC or platform.system() == _Keys.DARWIN:
+            get_cb = self.__mac_get_clipboard
+            set_cb = self.__mac_set_clipboard
+            self._set_data(
+                key=_Keys.COPY, value=set_cb, set_default_type=Optional[MethodType]
+            )
+            self._set_data(
+                key=_Keys.PASTE, value=get_cb, set_default_type=Optional[MethodType]
+            )
 
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+    def __mac_set_clipboard(self, text: str) -> None:
+        """Set MacOS clipboard data."""
+        text = str(text)
+        out_f: os._wrap_close = os.popen("pbcopy", "w")
+        out_f.write(text)
+        out_f.close()
+
+    def __mac_get_clipboard(self) -> str:
+        """Get MacOS clipboard data."""
+        out_f: os._wrap_close = os.popen("pbpaste", "r")
+        content: str = out_f.read()
+        out_f.close()
+        return content
 
 
-class _XClip(BData, _IClip):
+class _XClip(_BClip):
     """X11 clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
+    def __init__(self) -> None:
+        """Initialize the class."""
+        if os.name == _Keys.POSIX or platform.system() == _Keys.LINUX:
+            if os.system("which xclip > /dev/null") == 0:
+                get_cb = self.__xclip_get_clipboard
+                set_cb = self.__xclip_set_clipboard
+                self._set_data(
+                    key=_Keys.COPY, value=set_cb, set_default_type=Optional[MethodType]
+                )
+                self._set_data(
+                    key=_Keys.PASTE, value=get_cb, set_default_type=Optional[MethodType]
+                )
 
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+    def __xclip_set_clipboard(self, text: str) -> None:
+        """Set xclip clipboard data."""
+        text = str(text)
+        out_f: os._wrap_close = os.popen("xclip -selection c", "w")
+        out_f.write(text)
+        out_f.close()
+
+    def __xclip_get_clipboard(self) -> str:
+        """Get xclip clipboard data."""
+        out_f: os._wrap_close = os.popen("xclip -selection c -o", "r")
+        content: str = out_f.read()
+        out_f.close()
+        return content
 
 
-class _GtkClip(BData, _IClip):
+class _XSel(_BClip):
+    """X11 clipboard class."""
+
+    def __init__(self) -> None:
+        """Initialize the class."""
+        if os.name == _Keys.POSIX or platform.system() == _Keys.LINUX:
+            if os.system("which xsel > /dev/null") == 0:
+                get_cb = self.__xsel_get_clipboard
+                set_cb = self.__xsel_set_clipboard
+                self._set_data(
+                    key=_Keys.COPY, value=set_cb, set_default_type=Optional[MethodType]
+                )
+                self._set_data(
+                    key=_Keys.PASTE, value=get_cb, set_default_type=Optional[MethodType]
+                )
+
+    def __xsel_set_clipboard(self, text: str) -> None:
+        """Set xsel clipboard data."""
+        text = str(text)
+        out_f: os._wrap_close = os.popen("xsel -b -i", "w")
+        out_f.write(text)
+        out_f.close()
+
+    def __xsel_get_clipboard(self) -> str:
+        """Get xsel clipboard data."""
+        out_f: os._wrap_close = os.popen("xsel -b -o", "r")
+        content: str = out_f.read()
+        out_f.close()
+        return content
+
+
+class _GtkClip(_BClip):
     """Gtk clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
+    def __init__(self) -> None:
+        """Initialize the class."""
 
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+    def __gtk_get_clipboard(self) -> str:
+        """Get GTK clipboard data."""
+        return gtk.Clipboard().wait_for_text()  # type: ignore
+
+    def __gtk_set_clipboard(self, text: str) -> None:
+        """Set GTK clipboard data."""
+        global cb
+        text = str(text)
+        cb = gtk.Clipboard()  # type: ignore
+        cb.set_text(text)
+        cb.store()
 
 
-class _QtClip(BData, _IClip):
+class _QtClip(_BClip):
     """Qt clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
+    def __init__(self) -> None:
+        """Initialize the class."""
 
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+    def __qt_get_clipboard(self) -> str:
+        """Get QT clipboard data."""
+        return str(cb.text())
+
+    def __qt_set_clipboard(self, text: str) -> None:
+        """Set QT clipboard data."""
+        text = str(text)
+        cb.setText(text)
 
 
-class _TkClip(BData, _IClip):
+class _TkClip(_BClip):
     """Tk clipboard class."""
 
-    def get_clipboard(self) -> str:
-        """Get clipboard content."""
-        return ""
-
-    def set_clipboard(self, value: str) -> None:
-        """Set clipboard content."""
-        pass
+    def __init__(self) -> None:
+        """Initialize the class."""
 
 
 class ClipBoard(BData):
