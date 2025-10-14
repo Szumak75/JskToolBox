af775b4b5624fd19393c6a2a587a0817761240f5
Jacek 'Szumak' Kotlarski <szumak@vierthost.pl>
2025-10-14 22:08:24 +0200

    `Refactor logging subsystem: update engine, formatter, and queue classes`

--- jsktoolbox/logstool/__init__.py
+++ jsktoolbox/logstool/__init__.py
@@ -1,1 +1,47 @@
+"""
+Author:  Jacek Kotlarski --<szumak@virthost.pl>
+Created: 2023-09-04
 
+Purpose: Aggregate logging toolkit components (engines, formatters, queues).
+
+The package re-exports commonly used classes to simplify imports for
+applications utilising the JskToolBox logging subsystem.
+"""
+
+from .keys import LogKeys, LogsLevelKeys, SysLogKeys  # noqa: F401
+from .queue import LoggerQueue  # noqa: F401
+from .formatters import (  # noqa: F401
+    LogFormatterNull,
+    LogFormatterDateTime,
+    LogFormatterTime,
+    LogFormatterTimestamp,
+)
+from .engines import (  # noqa: F401
+    LoggerEngineStdout,
+    LoggerEngineStderr,
+    LoggerEngineFile,
+    LoggerEngineSyslog,
+)
+from .logs import (  # noqa: F401
+    LoggerClient,
+    LoggerEngine,
+    ThLoggerProcessor,
+)
+
+__all__ = [
+    "LogKeys",
+    "LogsLevelKeys",
+    "SysLogKeys",
+    "LoggerQueue",
+    "LogFormatterNull",
+    "LogFormatterDateTime",
+    "LogFormatterTime",
+    "LogFormatterTimestamp",
+    "LoggerEngineStdout",
+    "LoggerEngineStderr",
+    "LoggerEngineFile",
+    "LoggerEngineSyslog",
+    "LoggerClient",
+    "LoggerEngine",
+    "ThLoggerProcessor",
+]
